0b580927750e6680712ab994c49a3165f10cd52b
Khajidmaa <bhajaa4@gmail.com>
2021-08-13 14:52:26 +0800

    header.css файл бичиж дууслаа

--- postcss.config.js
+++ postcss.config.js
@@ -5,6 +5,7 @@ module.exports = {
     require("postcss-nested"),
     require("postcss-simple-vars"),
     require("postcss-preset-env"),
+    require("postcss-hexrgba"),
     require("autoprefixer"),
   ],
 };
